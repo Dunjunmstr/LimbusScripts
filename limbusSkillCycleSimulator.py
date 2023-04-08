b9e6f0f9cbcc3ecc6393aaf42cc7e56821ebaa9a
Dunjunmstr <dunjunmstr@gmail.com>
2023-04-08 15:53:21 -0700

    Fixed a cycle logic bug

--- limbusSkillCycleSimulator.py
+++ limbusSkillCycleSimulator.py
@@ -13,36 +13,19 @@ def drawBag(bag, drawCount, baseBag = SKILL_BAG):
 		bag = bag[0:-drawCount]
 		return results, bag
 
-def THREES_ONLY(top, bottom):
-	#Useful for Seven Ryoshu
-	if top == 3:
-		(top, bottom, use) = (None, bottom, top)
-	elif bottom == 3:
-		(top, bottom, use) = (None, top, bottom)
-	else: # No 3's detected, cycling both
-		(top, bottom, use) = (None, None, max(top, bottom))
-	return top, bottom, use
+def cycleLogicTemplate(takeCondition):
+	def func(top, bottom):
+		if takeCondition(top):
+			return (None, bottom, top)
+		elif takeCondition(bottom):
+			return (None, top, bottom)
+		else:
+			return (None, None, top)
+	return func		
 
-def TWOS_ONLY(top, bottom):
-	#Possibly useful for Shi Ishmael
-	if top == 2:
-		(top, bottom, use) = (None, bottom, top)
-	elif bottom == 2:
-		(top, bottom, use) = (None, top, bottom)
-	else: # No 2's detected, cycling both
-		(top, bottom, use) = (None, None, max(top, bottom))
-	return top, bottom, use
-
-def THROW_OUT_ONES(top, bottom):
-	#Default
-	if (top, bottom) != (1, 1):
-		(top, bottom, use) = (None, min(top, bottom), max(top, bottom))
-	else: # Double-ones, cycling both
-		assert (top == bottom)
-		assert (top == 1)
-		(top, bottom, use) = (None, None, max(top, bottom))
-	return top, bottom, use	
-
+THREES_ONLY = cycleLogicTemplate(lambda x: x == 3) #If there's a 3, take it. Useful for Seven Ryoshu
+TWOS_ONLY = cycleLogicTemplate(lambda x: x == 2) #If there's a 2, take it. Useful for Shi Ishmael/Hong Lu.
+THROW_OUT_ONES = cycleLogicTemplate(lambda x: x != 1) #If there's anything that isn't a 1, take it. Default behavior.
 
 def simulation(trials = 10000, cycleLogic = THREES_ONLY, cycleLogicName = " with only threes"):
 	totals = [0, 0, 0]
@@ -52,13 +35,13 @@ def simulation(trials = 10000, cycleLogic = THREES_ONLY, cycleLogicName = " with
 	for i in range(0, trials):
 		if bottom == None:
 			[top, bottom], currentBag = drawBag(currentBag, 2)
-		else: #Top is probably none here
+		else: #If bottom is none, top is none because old skills fall to the bottom
 			[top], currentBag = drawBag(currentBag, 1)
 		top, bottom, use = cycleLogic(top, bottom)
 		totals[use - 1] += 1
 	print("Out of %s trials%s, Skill 1 was used %s times, Skill 2 was used %s times, and Skill 3 was used %s times" % 
 				(trials, cycleLogicName, totals[0], totals[1], totals[2]))
 
-simulation(100000, THREES_ONLY, " with as many Skill 3s as possible")
-simulation(100000, TWOS_ONLY, " with as many Skill 2s as possible")
-simulation(100000, THROW_OUT_ONES, " with as many Skill 2s and 3s as possible")
+simulation(1000000, THREES_ONLY, " with as many Skill 3s as possible")
+simulation(1000000, TWOS_ONLY, " with as many Skill 2s as possible")
+simulation(1000000, THROW_OUT_ONES, " with as many Skill 2s and 3s as possible")
